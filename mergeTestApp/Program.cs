d50097fa0659ef83feeae2cf9962d682bfe847c2
Ruslan Krabobor <rkrabobor@gmcs.ru>
2017-07-05 16:16:57 +0300

    5

--- mergeTestApp/Program.cs
+++ mergeTestApp/Program.cs
@@ -19,6 +19,7 @@ namespace mergeTestApp
             Console.WriteLine("6!");
             Console.WriteLine("7!");
             Console.WriteLine("8!");
+            Console.WriteLine("9!");
 
             Console.ReadLine();
         }
